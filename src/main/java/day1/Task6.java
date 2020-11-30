32affadba296631292787a2cd1b6092bb57e4726
SalovS <sviatoslavsalov@yandex.ru>
2020-11-30 18:11:31 +0600

    Add tasks of the first day

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,13 @@
 package day1;
 
+import java.util.Random;
+
 public class Task6 {
     public static void main(String[] args) {
-
+        Random random = new Random();
+        int k = random.nextInt(9) +1;
+        for(int i = 1; i < 10; i++){
+            System.out.printf("%d x %d = %d\n",i ,k ,i*k);
+        }
     }
 }
