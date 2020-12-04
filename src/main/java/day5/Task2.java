4fcd23e1b83aebcc6ab13e1cf962d4b7dd22eb57
SalovS <sviatoslavsalov@yandex.ru>
2020-12-04 10:24:41 +0600

    add Tack day5 correction 1

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -8,27 +8,3 @@ public class Task2 {
                 motorbike.getModel(), motorbike.getColor(), motorbike.getYear());
     }
 }
-
-class Motorbike {
-    private int year;
-    private String color;
-    private String model;
-
-    public Motorbike(int year, String color, String model) {
-        this.year = year;
-        this.color = color;
-        this.model = model;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getModel() {
-        return model;
-    }
-}
