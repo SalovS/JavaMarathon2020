e0d4c777c7592deaacd143d31b4f680077ebdcf6
SalovS <sviatoslavsalov@yandex.ru>
2020-12-13 20:02:11 +0600

    Add Tack day12 correction 2

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -7,9 +7,9 @@ public class Task2 {
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<Integer>();
         for (int i = 0; i <= 350; i += 2) {
-            if(i > 30 && i < 300)
-                continue;
             numbers.add(i);
+            if(i == 30)
+                i = 298;
         }
         for (int number : numbers) {
             System.out.println(number);
