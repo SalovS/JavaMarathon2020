ccc85ee4e008fdada27913bb39a7f22adb13e6be
SalovS <sviatoslavsalov@yandex.ru>
2020-12-14 15:04:41 +0600

    Add Tack day14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,39 @@
 package day14;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        String separator = File.separator;
+        String path = "src" + separator +
+                "main" + separator +
+                "java" + separator +
+                "day14" + separator +
+                "text.txt";
+        File file = new File(path);
+        printSumDigits(file);
+    }
+
+    public static void printSumDigits(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum += Integer.parseInt(numbers[i]);
+                count++;
+            }
+            if (count == 10)
+                System.out.println(sum);
+            else
+                System.out.println("Некорректный входной файл");
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,43 @@
 package day14;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        String separator = File.separator;
+        String path = "src" + separator +
+                "main" + separator +
+                "java" + separator +
+                "day14" + separator +
+                "people.txt";
+        File file = new File(path);
+        System.out.println(Arrays.toString(parseFileToObjList(file).toArray()));
+    }
+    public static List<Person> parseFileToObjList(File file) {
+        List<Person> data = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] text = line.split(" ");
+                int age = Integer.parseInt(text[1]);
+                if (age < 0)
+                    try{
+                        throw new IOException();
+                    }catch (Exception e){
+                        System.out.println("Некорректный входной файл");
+                        return new ArrayList<>();
+                    }
+                data.add(new Person(text[0] ,age));
+            }
+        } catch (Exception e) {
+            System.out.println("Файл не найден");
+        }
+        return data;
     }
 }
